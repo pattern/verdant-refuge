aca2b2f61b8756f5ecc26c1ccba2cf39d2b7be4d
Jeff Pickelman <pickelman@gmail.com>
2012-09-03 15:41:32 -0700

    Change formatting of timeago on blog header

--- content/media/js/global.js
+++ content/media/js/global.js
@@ -1,7 +1,8 @@
 $(function() {
-  $('.timeago').each(function () {
+  $('span.timeago').each(function () {
     var date = new Date($(this).attr('title'));
     var timeago = $.timeago(date);
     $(this).html(timeago);
   });
+  $('time.timeago').timeago();
 });
