91ccbd2b7e7976cd01926de989471059a6bc37f1
Jeff Pickelman <pickelman@gmail.com>
2012-09-17 17:42:20 -0700

    Update "publish" Fabric function to log git commit and date

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 from fabric.api import local
+from datetime import datetime
 
 def clean():
   local('rm -rf ./deploy')
@@ -14,3 +15,12 @@ def serve():
 def publish():
   regen()
   local('hyde publish -p s3')
+  
+  # Log the most recent commit at time of publishing:
+  local('rm -f last-published.txt')
+  pub = "Verdant Refuge was last published: %s" % \
+        datetime.now()
+  local("git log --pretty=format:'Git: %s (%h, %aD)' -n 1 \
+         > last-published.txt")
+  local('echo "\n%s" | cat >> last-published.txt' % pub)
+
